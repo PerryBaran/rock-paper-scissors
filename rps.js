ea96d331ab3736b9e57f2d07986927eb2367134c
Perry <petreli1@ymail.com>
2021-08-31 11:57:27 +0100

    added function to compare players choice and computer choice and provide an appropriate message dependant upon the results

--- rps.js
+++ rps.js
@@ -2,19 +2,58 @@ function computerPlay() {
     let computerChoice = Math.floor(Math.random()*100); 
     
         if (computerChoice <= 33) {
-            return 'Rock';
+            return 'rock';
         }
     
         else if (computerChoice > 66) {
-            return 'Scissors';
+            return 'scissors';
         }
     
         else {
-            return 'Paper';
+            return 'paper';
         }
 }
 
 let playerPlay = prompt("Rock, Paper, Scissors?");
 let playerChoice = playerPlay.toLowerCase();
 
-console.log(playerChoice);
+const playerSelection = playerChoice;
+const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock beats scissors'
+    }
+
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors beats paper'
+    }
+
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper beats rock'
+    }
+
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lose! Paper beats rock'
+    }
+
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock beats scissors'
+    }
+
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Scissors beats paper'
+    }
+
+    else if (playerSelection === 'rock' && computerSelection === 'rock' || playerSelection === 'paper' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'Draw!'
+    }
+
+    else {
+        return 'unexpected input'
+    }
+
+}
+
+console.log (playRound(playerSelection, computerSelection))
