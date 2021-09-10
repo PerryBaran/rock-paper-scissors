9cb2abddc2fed04ed25625eefc50bd906b6637a3
Squilly-flac <84458310+Squilly-flac@users.noreply.github.com>
2021-09-10 13:05:29 +0100

    Add files via upload

--- rps.js
+++ rps.js
@@ -1,92 +1,128 @@
+let playerPoints = 0;
+let computerPoints = 0;
+
+const buttons = document.querySelectorAll('button');
+const playerChoice = document.querySelector('#PlayerChoice');
+const computerChoice= document.querySelector('#ComputerChoice');
+const playerScore = document.querySelector('#PlayerScore');
+const computerScore = document.querySelector('#ComputerScore');
+const winLose = document.querySelector('#WinLose');
+
 function computerPlay() {
     let computerChoice = Math.floor(Math.random()*100); 
     
         if (computerChoice <= 33) {
             return 'rock';
-        }
-    
+        }    
         else if (computerChoice > 66) {
             return 'scissors';
         }
-    
         else {
             return 'paper';
         }
 }
 
-function playerPlay() {
-    let input = prompt("Rock, Paper, Scissors?");
-    const playerInput = input.toLowerCase();
-    return playerInput
-}
+function playersImage(choice) {
+    var playersImg = new Image();
 
-function playRound() {
+    if (choice === 'rock') {
+        playersImg.src = 'rpsimages/Rock.png';
+        document.getElementById('PlayerChoice').appendChild(playersImg);
+    }
+    else if (choice === 'paper') {
+        playersImg.src = 'rpsimages/Paper.png';
+        document.getElementById('PlayerChoice').appendChild(playersImg);
+    }
+    else if (choice === 'scissors') {
+        playersImg.src = 'rpsimages/Scissors.png';
+        document.getElementById('PlayerChoice').appendChild(playersImg);
+    }
+};
+
+function computerImage(computerSelection) {
+    var computersImg = new Image();
+    
+    if (computerSelection === 'rock') {
+        computersImg.src = 'rpsimages/Rock.png';
+        document.getElementById('ComputerChoice').appendChild(computersImg);
+    }
+    else if (computerSelection === 'paper') {
+        computersImg.src = 'rpsimages/Paper.png';
+        document.getElementById('ComputerChoice').appendChild(computersImg);
+    }
+    else if (computerSelection === 'scissors') {
+        computersImg.src = 'rpsimages/Scissors.png';
+        document.getElementById('ComputerChoice').appendChild(computersImg);
+    }
+
+    computersImg.classList.add('flip');
+};
+
+function playRound(playerSelection) {
     let computerSelection = computerPlay();
-    let playerSelection = playerPlay();
+    computerImage(computerSelection);
+    
 
     if ((playerSelection === 'rock' && computerSelection === 'scissors')
      || (playerSelection === 'scissors' && computerSelection === 'paper')
      || (playerSelection === 'paper' && computerSelection === 'rock')) {
-        result = 'You win, ' + playerSelection + ' beats ' + computerSelection;
-    }
 
-    else if ((playerSelection === 'rock' && computerSelection === 'paper')
+        ++playerPoints;
+        playerScore.innerHTML = playerPoints;
+        playerScore.style.backgroundColor = 'chartreuse';
+        computerScore.style.backgroundColor = '#45e9ff';
+    }
+    else if ((playerSelection === 'rock' && computerSelection=== 'paper')
     || (playerSelection === 'scissors' && computerSelection === 'rock') 
     || (playerSelection === 'paper' && computerSelection === 'scissors')) {
-        result = 'You lose, ' + computerSelection + ' beats ' + playerSelection;
-    }
 
-    else if (playerSelection ===  computerSelection) {
-        result = 'Draw, both players chose ' + playerSelection;
+        ++computerPoints
+        computerScore.innerHTML = computerPoints;
+        computerScore.style.backgroundColor = 'chartreuse';
+        playerScore.style.backgroundColor = '#45e9ff';
     }
-
     else {
-        result = 'unexpected input'
+        computerScore.style.backgroundColor = '#45e9ff';
+        playerScore.style.backgroundColor = '#45e9ff';
     }
-
-    console.log(result);
-
-    return result;
 }
 
-
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    let round = 5;
-
-    for (var i=0; i<round; i++) {
-        playRound();
-
-        if (result.includes('win')) {
-            ++playerPoints;
-        }
-        else if (result.includes('lose')) {
-            ++computerPoints;
-        }
-
-        else if (result.includes('unexpected')) {
-            ++round;
-        }
-
-        console.log('Your score: ' + playerPoints, '\nComputers score: ' + computerPoints)
+function resetCounter() {
+    
+    if (playerPoints === 0 && computerPoints == 0) {
+    playerScore.innerHTML = '0';
+    computerScore.innerHTML = '0';
+    winLose.innerHTML = ' ';
     }
-    
-    let finalResult;
-
-        if (playerPoints > computerPoints) {
-            finalResult = 'You Won the game! ' +  '\nYour score: ' + playerPoints + '\nComputers score: ' + computerPoints;
-        }
-
-        else if (playerPoints < computerPoints) {
-            finalResult = 'You Lost the game! ' +  '\nYour score: ' + playerPoints + '\nComputers score: ' + computerPoints;
-        }
-
-        else {
-            finalResult = 'You drew the game!  ' +  '\nYour score: ' + playerPoints + '\nComputers score: ' + computerPoints;
-        }
-    
-    return finalResult;
 }
 
-console.log(game())
+function playGame(playerSelection) {
+    resetCounter();
+    playRound(playerSelection);
+
+    if (playerPoints === 5) {
+        winLose.innerHTML = 'YOU WIN!';
+        playerPoints = 0;
+        computerPoints = 0;
+    }
+    else if (computerPoints === 5) {
+        winLose.innerHTML = 'YOU LOSE!';
+        playerPoints = 0;
+        computerPoints = 0;
+    }
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playGame(button.id);
+
+    
+
+        var element = document.querySelector('img');
+	    element.parentNode.removeChild(element);
+        var element = document.querySelector('img');
+	    element.parentNode.removeChild(element);
+
+        playersImage(button.id);
+    });
+});
